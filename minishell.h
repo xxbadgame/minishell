9ab4a8a46fa1f78bd7de65255ceea65d55c6195d
Enzo Giuseppin <engiusep@z3r8p3.42lyon.fr>
2025-04-14 15:58:22 +0200

    test

---
+++ minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: engiusep <engiusep@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 13:18:09 by engiusep          #+#    #+#             */
+/*   Updated: 2025/04/14 15:01:08 by engiusep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>  
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <curses.h>   
