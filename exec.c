4278515c485b4f8513dfbfd45836c4e30a1a6313
xxbadgame <yannis.nzuepro@gmail.com>
2025-04-22 17:59:17 +0200

    basics exec

--- exec.c
+++ exec.c
@@ -6,36 +6,36 @@
 /*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 14:56:08 by yannis            #+#    #+#             */
-/*   Updated: 2025/04/22 16:20:46 by yannis           ###   ########.fr       */
+/*   Updated: 2025/04/22 17:57:38 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "exec.h"
 
-/*
-void execute(char **cmds)
+int get_path_command()
 {
-    char	*pathname;
-    int		pid;
-
-	pid = fork();
-	if (pid < 0)
-		return (-1);
-	else if (pid == 0)
-	{
-		if (execve("/bin/ls", ))
-	}
-}
-*/
-
-int main() {
-    char *argv[] = {"/bin/ls", "-l", NULL};
-    char *envp[] = {NULL};
-
-    if (execve("/bin/ls", argv, envp) == -1) {
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-
+	char *path = getenv("PATH");
+	if (path)
+		printf("PATH = %s\n", path);
+	else
+		printf("PATH variable not found\n");
     return 0;
+
+}
+
+int	execute_command(char **cmd, char **envp)
+{
+	get_path_command();
+	if (execve("/bin/ls", cmd, envp) == -1)
+	{
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
+
+int main(char **envp) 
+{
+	char **cmd = {"ls", "-l", "-a", NULL}; 
+	execute_command(cmd, envp);
 }
