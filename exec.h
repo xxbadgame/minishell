495a72306ea09d8733e449859719c4969b153dc9
xxbadgame <yannis.nzuepro@gmail.com>
2025-05-06 10:56:24 +0200

    branche

--- exec.h
+++ exec.h
@@ -6,7 +6,7 @@
 /*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 14:57:12 by yannis            #+#    #+#             */
-/*   Updated: 2025/04/27 10:36:46 by yannis           ###   ########.fr       */
+/*   Updated: 2025/05/06 10:55:24 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,17 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 
+typedef struct s_cmd
+{
+	char			**argv;
+	char			*infile;
+	char			*outfile;
+	int				append;
+	struct s_cmd	*next;
+
+}					t_cmd;
+
 void free_split(char **split);
+int	execute_command(char **cmd, char **envp);
 
 #endif
--- pipeline.c
+++ pipeline.c
@@ -6,10 +6,23 @@
 /*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 13:19:31 by yannis            #+#    #+#             */
-/*   Updated: 2025/04/23 13:20:26 by yannis           ###   ########.fr       */
+/*   Updated: 2025/05/06 10:55:48 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// mettre le wait ici car sinon les commandes attendes les autres pour travailler
-// il faut le mettre dans la focntion générale pipe pour permettre l'execution parallèle 
-// waitpid(pid, NULL, 0);
+#include "exec.h"
+
+int pipeline(t_cmd **cmds, char **envp)
+{
+    int pipe[2];
+    t_cmd *cmd;
+    
+    cmd = *cmds;
+    while (cmd != NULL)
+    {
+        execute_command(cmd, envp);
+        
+        cmd = cmd->next;
+    }
+    
+}
