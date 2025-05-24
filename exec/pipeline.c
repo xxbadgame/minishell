1572f7622ac865c14e60784a79a685aca9f35bc9
xxbadgame <yannis.nzuepro@gmail.com>
2025-05-24 16:29:42 +0200

    42

--- exec/pipeline.c
+++ exec/pipeline.c
@@ -6,47 +6,11 @@
 /*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 09:13:39 by engiusep          #+#    #+#             */
-/*   Updated: 2025/05/24 11:13:35 by yannis           ###   ########.fr       */
+/*   Updated: 2025/05/24 16:26:32 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../terminal.h"
-
-int command_pipeline_basic(t_cmd *cmd, int in_fd, int *pipefd, t_shell *shell)
-{
-    if(cmd->infile)
-        in_fd = open(cmd->infile, O_RDONLY);
-    if (in_fd != 0) {
-        dup2(in_fd, 0);
-        close(in_fd);
-    }
-    if (cmd->next) {
-        dup2(pipefd[1], 1);
-        close(pipefd[0]);
-        close(pipefd[1]);
-    }
-    launch_execve(cmd, shell->env);
-    return(0);
-}
-
-int command_pipeline_builtins(t_cmd *cmd, int in_fd, int *pipefd,t_shell *shell)
-{
-    
-    if(cmd->infile)
-        in_fd = open(cmd->infile, O_RDONLY);
-    if (in_fd != 0) {
-        dup2(in_fd, 0);
-        close(in_fd);
-    }
-    if (cmd->next) {
-        dup2(pipefd[1], 1);
-        close(pipefd[0]);
-        close(pipefd[1]);
-    }
-    if(exec_builtin(cmd,shell) == -1)
-        return(-1);
-    return(0);
-}
 
 int pipeline(t_shell *shell)
 {
@@ -71,14 +35,34 @@ int pipeline(t_shell *shell)
         pid = fork();
         if (pid == 0)
         {
+            if (cmd->infile != NULL && cmd->heredoc == 0)
+			    redirect_left(cmd->infile);
+            else if (cmd->infile != NULL && cmd->heredoc == 1)
+                heredoc(cmd->infile);
+            else if (in_fd != 0)
+            {
+                dup2(in_fd, 0);
+                close(in_fd);
+            }
+            if (cmd->outfile != NULL && cmd->append == 0)
+                redirect_right(cmd->outfile);
+            else if (cmd->outfile != NULL && cmd->append == 1)
+                double_redirect_right(cmd->outfile);
+            else if (cmd->next)
+            {
+                dup2(pipefd[1], 1);
+                close(pipefd[1]);
+                close(pipefd[0]);
+            }
             if (is_builtin(cmd) == 0)
-                command_pipeline_basic(cmd, in_fd, pipefd,shell);
+               launch_execve(cmd, shell->env);
             else
-                command_pipeline_builtins(cmd, in_fd, pipefd,shell);
+                exec_builtin(cmd, shell);
+            exit(0);
         }
         if (in_fd != 0)
             close(in_fd);
-        if (cmd->next) 
+        if (cmd->next)
         {
             close(pipefd[1]);
             in_fd = pipefd[0];
@@ -86,12 +70,5 @@ int pipeline(t_shell *shell)
         cmd = cmd->next;
     }
     while (wait(NULL) > 0);
-    if (cmd->outfile != NULL && cmd->append == 0)
-    {
-        if(redirect_right(cmd->outfile))
-            perror("redirect : ");
-    }
-    else if (cmd->outfile != NULL && cmd->append == 1)
-        double_redirect_right(cmd->outfile);
     return 0;
 }
