e1ef11eac49fe7a3e94bbbb7a89b932c17815dde
engiusep <enzo74giuseppin@gmail.com>
2025-06-21 11:59:01 +0000

    fix exit

--- builtins/builtin_exit.c
+++ builtins/builtin_exit.c
@@ -14,6 +14,8 @@
 
 int	builtin_exit(t_shell *shell)
 {
+	int test;
+	test = shell->last_exit;
 	printf("exit\n");
 	if(!shell->cmds->cmd_args[1])
 	{
@@ -21,29 +23,31 @@ int	builtin_exit(t_shell *shell)
 		free_tokens(shell);
 		free_cmds(shell);
 		free_env(shell);
+		test = shell->last_exit; 
 		free(shell);
-		exit(shell->last_exit);
+		exit(test);
 	}
 	else if (ft_isnum(shell->cmds->cmd_args[1])
 		|| ft_atoi(shell->cmds->cmd_args[1]) == -1)
 	{
 		print_error("minishell: exit:", shell->cmds->cmd_args[1],
 			" numeric argument required\n");
-		shell->last_exit = 2;
+		test = 2;
 	}
 	else if(shell->cmds->cmd_args[2])
 	{
 		print_error("minishell: exit:", shell->cmds->cmd_args[1],
 			" too many argument\n");
-		shell->last_exit = 1;
+		test = 1;
 		
 	}
 	else
-		shell->last_exit = ft_atoi(shell->cmds->cmd_args[1]) % 256;
+		test = ft_atoi(shell->cmds->cmd_args[1]) % 256;
 	free(shell->line);
 	free_tokens(shell);
 	free_cmds(shell);
 	free_env(shell);
 	free(shell);
-	exit(shell->last_exit);
+	exit(test);
 }
+
