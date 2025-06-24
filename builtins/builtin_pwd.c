4d62c244fdbdd40031adf113bd7e5411ada8541b
xxbadgame <yannis.nzuepro@gmail.com>
2025-06-24 17:03:39 +0200

    no fix <<

--- builtins/builtin_pwd.c
+++ builtins/builtin_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: engiusep <engiusep@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 09:59:49 by engiusep          #+#    #+#             */
-/*   Updated: 2025/06/05 09:41:02 by engiusep         ###   ########.fr       */
+/*   Updated: 2025/06/24 16:48:43 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,18 @@ int	builtin_pwd(void)
 	char	*cwd;
 
 	cwd = getcwd(NULL, 0);
-	printf("%s\n", cwd);
+	if (!cwd)
+	{
+		perror("getcwd");
+		return (1);
+	}
+	if (write(STDOUT_FILENO, cwd, strlen(cwd)) == -1
+		|| write(STDOUT_FILENO, "\n", 1) == -1)
+	{
+		perror("write");
+		free(cwd);
+		return (1);
+	}
 	free(cwd);
 	return (0);
 }
