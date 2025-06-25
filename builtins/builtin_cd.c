228fde0765aeade696ee939d3f407584d2bd5a34
engiusep <enzo74giuseppin@gmail.com>
2025-06-25 09:31:11 +0200

    fix cd

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
+/*   By: engiusep <engiusep@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:20:00 by engiusep          #+#    #+#             */
-/*   Updated: 2025/06/24 10:53:10 by yannis           ###   ########.fr       */
+/*   Updated: 2025/06/25 09:30:24 by engiusep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,15 @@ static void	handle_pwd(t_env *env)
 static int	check_path_cmd(t_cmd *cmd)
 {
 	if (cmd->cmd_args[1] == NULL)
-		return (-1);
+		return (0);
 	if (cmd->cmd_args[2] != NULL)
-		return (ft_putendl_fd("cd : to many arguments\n", 2), -1);
+		return (ft_putendl_fd("cd : to many arguments", 2), 1);
 	if (cmd->cmd_args[1] != NULL)
 	{
 		if (cmd->cmd_args[1][0] == '/')
 		{
 			if (chdir(cmd->cmd_args[1]) == -1)
-				return (perror("dir"), -1);
+				return (perror("dir"), 1);
 			return (1);
 		}
 	}
@@ -75,9 +75,7 @@ int	builtin_cd(t_cmd *cmd, t_env *env)
 	char	*new_path;
 	char	*temp;
 
-	if (check_path_cmd(cmd) == -1)
-		return (-1);
-	else if (check_path_cmd(cmd) == 1)
+	if (check_path_cmd(cmd) == 1)
 		return (0);
 	cwd = getcwd(NULL, 0);
 	handle_oldpwd(cwd, env);
@@ -93,7 +91,7 @@ int	builtin_cd(t_cmd *cmd, t_env *env)
 	if (!new_path)
 		return (free(cwd), -1);
 	if (chdir(new_path) == -1)
-		return (free(cwd), free(new_path), perror("dir"), -1);
+		return (free(cwd), free(new_path), perror("dir"), 0);
 	handle_pwd(env);
 	return (free(new_path), free(cwd), 0);
 }
