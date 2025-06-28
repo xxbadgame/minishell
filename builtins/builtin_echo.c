a104c95c57fb75785b02e01c3b03258d704a4bf0
xxbadgame <yannis.nzuepro@gmail.com>
2025-06-28 13:02:41 +0200

    fix echo -n

--- builtins/builtin_echo.c
+++ builtins/builtin_echo.c
@@ -3,44 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_echo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: engiusep <engiusep@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yannis <yannis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 08:57:37 by engiusep          #+#    #+#             */
-/*   Updated: 2025/06/25 10:42:59 by engiusep         ###   ########.fr       */
+/*   Updated: 2025/06/28 13:01:59 by yannis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../terminal.h"
 
-static void	print_echo_args(t_cmd *cmd, int i, int min_index)
+int	is_echo_n(const char *s)
 {
+	int	i = 0;
+	if (s[i] != '-')
+		return (0);
+	i++;
+	if (s[i] != 'n')
+		return (0);
+	while (s[i] == 'n')
+		i++;
+	return (s[i] == '\0');
+}
+
+static void	print_echo_args(t_cmd *cmd, int i)
+{
+	int	first = 1;
 	while (cmd->cmd_args[i])
 	{
-		if (i > min_index)
-			if (write(1, " ", 1) == -1)
-				return ;
-		if (write(1, cmd->cmd_args[i], strlen(cmd->cmd_args[i])) == -1)
-			return ;
+		if (!first)
+			write(1, " ", 1);
+		write(1, cmd->cmd_args[i], strlen(cmd->cmd_args[i]));
+		first = 0;
 		i++;
 	}
 }
 
 int	builtin_echo(t_cmd *cmd)
 {
-	int	i;
+	int	i = 1;
+	int	newline = 1;
 
-	i = 1;
-	if (!cmd->cmd_args[1])
+	while (cmd->cmd_args[i] && is_echo_n(cmd->cmd_args[i]))
 	{
-		write(1, "\n", 1);
-		return (0);
+		newline = 0;
+		i++;
 	}
-	if (ft_strncmp(cmd->cmd_args[1], "-n", 2) == 0 && i++)
-		print_echo_args(cmd, i, 2);
-	else
-	{
-		print_echo_args(cmd, i, 1);
+	print_echo_args(cmd, i);
+	if (newline)
 		write(1, "\n", 1);
-	}
 	return (0);
 }
